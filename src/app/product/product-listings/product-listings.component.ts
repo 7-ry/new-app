baecbc0702d71c3f13d1f700055e906071baf082
Ryu14n <ryushin0714@gmail.com>
2025-03-03 17:19:27 -0800

    フロントエンドとバックエンドの結合

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { products } from '../../products';
+import { ProductService } from '../shared/product.service';
 
 
 @Component({
@@ -11,9 +11,16 @@ export class ProductListComponent implements OnInit {
 
   products: any
 
-  constructor() { }
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
-    this.products = products
+
+    const productObservable = this.productService.getProducts()
+    productObservable.subscribe(
+      (data) => {
+        this.products = data
+      },
+      (err) => { console.error('次のエラーが発生しました: ' + err) }
+    )
   }
 }
